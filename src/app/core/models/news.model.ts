0caab6156138045456afc9b26d484eebc79eefd7
Javier Reig Pascual <javrei@MANPOWER.ES>
2020-03-23 09:01:26 +0100

    adding dates

--- src/app/core/models/news.model.ts
+++ src/app/core/models/news.model.ts
@@ -3,12 +3,18 @@ export default class News {
     title: string;
     subtitle: string;
     description: string;
+    fecAlta: string;
 
-    constructor(id: string, title: string, subtitle: string, description: string ) {
+    constructor(id: string,
+                title: string,
+                subtitle: string,
+                description: string,
+                fecAlta: string ) {
       this.id = id;
       this.title = title;
       this.subtitle = subtitle;
       this.description = description;
+      this.fecAlta = fecAlta;
     }
 
   }
